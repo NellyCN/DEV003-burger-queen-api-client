f7143977298cd63c08ad3859256ca5fe84ce75ef
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-04-24 11:02:05 -0400

    inicio de modulos mesero, prueba root.render

--- burger-queen/src/components/componeteMesero.js
+++ burger-queen/src/components/componeteMesero.js
@@ -9,11 +9,11 @@ function Nav() {
           src={require('../images/logoheader.png')} />
           <div className='contenedor-links'>
         <nav className='links-navegacion'>
-          <h1 className='titulo-menu'>Burge Queen Menu</h1>
+          <h1 className='titulo-menu'>Burger Queen</h1>
           <ul>
-            <li> <a href="http://localhost:3000/"> Administrador </a></li>
-            <li><a href="http://localhost:3000/">Mesero </a></li>
-            <li><a href="http://localhost:3000/"> Cocina </a></li>
+            <li> <a href="http://localhost:3000/"> Administrador</a></li>
+            <li><a href="http://localhost:3000/">Mesero</a></li>
+            <li><a href="http://localhost:3000/"> Cocina</a></li>
           </ul>
         </nav>
         </div>
